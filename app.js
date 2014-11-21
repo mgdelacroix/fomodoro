01112d88f631b6eecb678d41f91ac954c5a40db5
Miguel de la Cruz <mgdelacroix@gmail.com>
2014-11-22 00:14:19 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+var app = {};
+
+app.getTimer = function() {
+    return document.getElementById("timer");
+};
+
+app.getTimerMinutes = function() {
+    var timer = app.getTimer().innerHTML,
+        minutes = timer.split(":")[0];
+    return parseInt(minutes);
+};
+
+app.pad2 = function(num) {
+    if (num < 10) {
+        return "0"+num;
+    } else {
+        return ""+num;
+    };
+};
+
+app.startTimer = function() {
+    var now = new Date(),
+        milisecondsToAdd = 1000*60*app.getTimerMinutes(),
+        finishTime = new Date(now.getTime()+milisecondsToAdd),
+        processTimestamp = function(ts) {
+            app.updateTimer(ts.minutes, ts.seconds);
+            if (ts.seconds == 0 &&
+                ts.minutes == 0 &&
+                ts.hours == 0) {
+                app.stopTimer();
+                app.playAudio();
+            };
+        };
+
+    app.cntdwn = countdown(processTimestamp,
+                           finishTime,
+                           countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
+};
+
+app.updateTimer = function(minutes, seconds) {
+    var timer = app.getTimer();
+    timer.innerHTML = app.pad2(minutes) + ":" + app.pad2(seconds);
+};
+
+app.stopTimer = function() {
+    window.clearInterval(app.cntdwn);
+    app.setMessage("Pommodoro finished!");
+};
+
+app.setTimer = function() {
+    var timer = app.getTimer();
+    var expected_timer = prompt("Write your expected time");
+    timer.innerHTML = app.pad2(expected_timer) + ":00";
+};
+
+app.getMessage = function() {
+    return document.getElementById("message");
+};
+
+app.setMessage = function(msg) {
+    var message = app.getMessage();
+    message.innerHTML = msg;
+};
+
+app.clearMessage = function() {
+    var message = app.getMessage();
+    message.innerHTML = null;
+};
+
+app.getAudio = function() {
+    return document.getElementById("finishAudio");
+};
+
+app.playAudio = function() {
+    var audio = app.getAudio();
+
+    audio.play();
+    window.setInterval(function() {audio.pause()}, 2000);
+    audio.fastSeek(0);
+};
+
+
+// Init
+var startTimer = document.getElementById("startTimer"),
+    setTimer = document.getElementById("setTimer");
+
+startTimer.onclick = app.startTimer;
+setTimer.onclick = app.setTimer;
